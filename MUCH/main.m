5cf9f3481f9388092f361d5a571058b807dcf1aa
wobushigogo <wangy322@sina.com>
2014-11-18 10:56:27 +0800

    Initial Commit

---
+++ MUCH/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MUCH
+//
+//  Created by 汪洋 on 14/11/18.
+//  Copyright (c) 2014年 wy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
