4ffe1ea9f6038ec9b85669e66bb938008f390927
wobushigogo <wangy322@sina.com>
2014-11-24 13:21:59 +0800

    wy

--- MUCH/ReleaseView/ReleaseViewController.m
+++ MUCH/ReleaseView/ReleaseViewController.m
@@ -8,7 +8,8 @@
 
 #import "ReleaseViewController.h"
 #import "AppDelegate.h"
-@interface ReleaseViewController ()
+#import "SliderViewController.h"
+@interface ReleaseViewController ()<UITextFieldDelegate>
 
 @end
 
@@ -18,10 +19,23 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor whiteColor];
+    
+    [SliderViewController sharedSliderController].canMoveWithGesture = NO;
+    
     [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationSlide];
     UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 320, 320)];
     imageView.image = self.image;
     [self.view addSubview:imageView];
+    
+    priceTextField = [[UITextField alloc] initWithFrame:CGRectMake(0, 322, 320, 60)];
+    priceTextField.backgroundColor = [UIColor grayColor];
+    priceTextField.placeholder = @"输入价格";
+    priceTextField.textAlignment = NSTextAlignmentCenter;
+    priceTextField.keyboardType = UIKeyboardTypeDecimalPad;
+    priceTextField.font =  [UIFont systemFontOfSize:15];
+    [priceTextField setClearButtonMode:UITextFieldViewModeWhileEditing];
+    priceTextField.delegate = self;
+    [self.view addSubview:priceTextField];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -29,14 +43,83 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void)viewWillAppear:(BOOL)animated
+{
+    [self registerForKeyboardNotifications];
 }
-*/
 
+-(void)viewWillDisappear:(BOOL)animated
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)registerForKeyboardNotifications
+{
+    //使用NSNotificationCenter 鍵盤出現時
+    [[NSNotificationCenter defaultCenter] addObserver:self
+     
+                                             selector:@selector(keyboardWasShown:)
+     
+                                                 name:UIKeyboardDidShowNotification object:nil];
+    
+    //使用NSNotificationCenter 鍵盤隐藏時
+    [[NSNotificationCenter defaultCenter] addObserver:self
+     
+                                             selector:@selector(keyboardWillBeHidden:)
+     
+                                                 name:UIKeyboardWillHideNotification object:nil];
+    
+    
+}
+
+//实现当键盘出现的时候计算键盘的高度大小。用于输入框显示位置
+- (void)keyboardWasShown:(NSNotification*)aNotification
+{
+    CGRect rect = [aNotification.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    CGFloat ty = - rect.size.height;
+    [UIView animateWithDuration:[aNotification.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue] animations:^{
+        self.view.transform = CGAffineTransformMakeTranslation(0, ty+183);
+    }];
+    
+    bgBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    bgBtn.frame = self.view.frame;
+    [bgBtn addTarget:self action:@selector(closeBtn) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:bgBtn];
+}
+
+//当键盘隐藏的时候
+- (void)keyboardWillBeHidden:(NSNotification*)aNotification
+{
+    //do something
+    [UIView animateWithDuration:[aNotification.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue] animations:^{
+        self.view.transform = CGAffineTransformIdentity;
+    }];
+    
+    [bgBtn removeFromSuperview];
+    bgBtn = nil;
+}
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    return [self validateNumber:string];
+}
+
+- (BOOL)validateNumber:(NSString*)number {
+    BOOL res = YES;
+    NSCharacterSet* tmpSet = [NSCharacterSet characterSetWithCharactersInString:@"0123456789"];
+    int i = 0;
+    while (i < number.length) {
+        NSString * string = [number substringWithRange:NSMakeRange(i, 1)];
+        NSRange range = [string rangeOfCharacterFromSet:tmpSet];
+        if (range.length == 0) {
+            res = NO;
+            break;
+        }
+        i++;
+    }
+    return res;
+}
+
+-(void)closeBtn{
+    [priceTextField resignFirstResponder];
+}
 @end
