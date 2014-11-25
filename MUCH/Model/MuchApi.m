8a8759adaeec776c16e53b338378d8a26184270e
wobushigogo <wangy322@sina.com>
2014-11-25 17:06:26 +0800

    wy

--- MUCH/Model/MuchApi.m
+++ MUCH/Model/MuchApi.m
@@ -12,6 +12,7 @@
 #import "ListModel.h"
 #import "userModel.h"
 #import "AttentionModel.h"
+#import "JSONKit.h"
 @implementation MuchApi
 //获取列表
 + (NSURLSessionDataTask *)GetListWithBlock:(void (^)(NSMutableArray *posts, NSError *error))block start:(int)start indexSize:(int)indexSize log:(NSString *)log lat:(NSString *)lat{
@@ -208,6 +209,7 @@
     }];
 }
 
+//登录
 + (NSURLSessionDataTask *)LoginWithBlock:(void (^)(NSMutableArray *posts, NSError *error))block userName:(NSString *)userName passWord:(NSString *)passWord{
     NSString *urlStr = [NSString stringWithFormat:@"user/signin"];
     NSDictionary *parametersdata = [[NSDictionary alloc] initWithObjectsAndKeys:
@@ -232,5 +234,68 @@
             block([NSMutableArray array], error);
         }
     }];
+}
+
+//注册
++ (NSURLSessionDataTask *)RegisterWithBlock:(void (^)(NSMutableArray *posts, NSError *error))block userName:(NSString *)userName passWord:(NSString *)passWord passwordConfirmation:(NSString *)passwordConfirmation avatar:(NSString *)avatar nickName:(NSString *)nickName{
+    NSString *urlStr = [NSString stringWithFormat:@"user/register"];
+    NSDictionary *parametersdata = [[NSDictionary alloc] initWithObjectsAndKeys:
+                                    userName,@"username",
+                                    passWord,@"password",
+                                    passwordConfirmation,@"passwordConfirmation",
+                                    avatar,@"avatar",
+                                    nickName,@"nickname",
+                                    nil];
+    NSLog(@"parametersdata ===> %@",parametersdata);
+    return [[AFAppDotNetAPIClient sharedClient] POST:urlStr parameters:parametersdata success:^(NSURLSessionDataTask * __unused task, id JSON) {
+        NSLog(@"JSON===>%@",JSON);
+        if([[NSString stringWithFormat:@"%@",JSON[@"status"][@"code"]]isEqualToString:@"200"]){
+            NSMutableArray *mutablePosts = [[NSMutableArray alloc] init];
+            [mutablePosts addObject:JSON[@"result"]];
+            if (block) {
+                block([NSMutableArray arrayWithArray:mutablePosts], nil);
+            }
+        }else{
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"提示" message:JSON[@"status"][@"text"] delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil, nil];
+            [alert show];
+        }
+    } failure:^(NSURLSessionDataTask *__unused task, NSError *error) {
+        NSLog(@"error ==> %@",error);
+        if (block) {
+            block([NSMutableArray array], error);
+        }
+    }];
+}
+
+//验证微信
++(void)GetWeiXin:(void (^)(NSDictionary *posts, NSError *error))block code:(NSString *)code{
+    NSURL *url=[NSURL URLWithString:[NSString stringWithFormat:@"https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx2fe5e9a05cc63f07&secret=03abac544342b22288ae0fdf5a05d630&code=%@&grant_type=authorization_code",code]];//创建URL
+    NSMutableURLRequest *request=[[NSMutableURLRequest alloc]initWithURL:url];//通过URL创建网络请求
+    [request setTimeoutInterval:30];//设置超时时间
+    [request setHTTPMethod:@"GET"];//设置请求方式
+    NSError *err;
+    NSData *data=[NSURLConnection sendSynchronousRequest:request returningResponse:nil error:&err];
+    //NSString *str=[[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
+    NSDictionary *resultDict = [data objectFromJSONData];
+    //NSLog(@"temp is :%@",resultDict);
+    if(block){
+        block([NSDictionary dictionaryWithDictionary:resultDict],nil);
+    }
+}
+
+//微信个人信息
++ (void)GetWeiXinUser:(void (^)(NSDictionary *dic, NSError *error))block access_token:(NSString *)access_token{
+    NSURL *url=[NSURL URLWithString:[NSString stringWithFormat:@"https://api.weixin.qq.com/sns/userinfo?access_token=%@&openid=OPENID",access_token]];//创建URL
+    NSMutableURLRequest *request=[[NSMutableURLRequest alloc]initWithURL:url];//通过URL创建网络请求
+    [request setTimeoutInterval:30];//设置超时时间
+    [request setHTTPMethod:@"GET"];//设置请求方式
+    NSError *err;
+    NSData *data=[NSURLConnection sendSynchronousRequest:request returningResponse:nil error:&err];
+    //NSString *str=[[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
+    NSDictionary *resultDict = [data objectFromJSONData];
+    NSLog(@"temp is :%@",resultDict);
+    if(block){
+        block([NSDictionary dictionaryWithDictionary:resultDict],nil);
+    }
 }
 @end
